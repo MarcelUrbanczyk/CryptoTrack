e916cced8d94c9ea5160a7a58b7116c6997f1e20
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-22 22:34:52 +0200

    created get coins function

---
+++ src/features/Browse/getCoins.ts
@@ -0,0 +1,14 @@
+import { options } from "../../options";
+
+export const getCoins = async () => {
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&page=1`,
+    options
+  );
+
+  if (response.status !== 200) {
+    return "error";
+  }
+
+  return await response.json();
+};
