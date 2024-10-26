eac6efbbc45fa3fde593b9f511fe88c08669aac6
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-26 15:53:44 +0200

    changed colors and created separate light and dark theme

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -1,17 +1,32 @@
 export const colors = {
-  dodgerBlue: "#5271ff",
-  lavenderPurple: "#8f8cb8",
-  magnolia: "#f9f8ff",
-  sisal: "#d5cabd",
-  black: "#000000",
+  federalBlue: "#0E0E52",
+  royalBlue: "#5d62e3",
+  dullLavender: "#9193e2",
+  whiteLilac: "#f6f6fb",
+  blackPearl: "#040520",
+  perano: "#acacf1",
+  luckyPoint: "#1d1e6d",
+  jacksonsPurple: "#1b20a1",
+  cinder: "#05050a",
+  selago: "#dfe0fb",
 };
 
-export const theme = {
+export const lightTheme = {
   colors: {
-    primary: colors.dodgerBlue,
-    secondary: colors.lavenderPurple,
-    background: colors.magnolia,
-    bonus: colors.sisal,
-    text: colors.black,
+    primary: colors.federalBlue,
+    secondary: colors.dullLavender,
+    background: colors.whiteLilac,
+    accent: colors.royalBlue,
+    text: colors.blackPearl,
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    primary: colors.perano,
+    secondary: colors.luckyPoint,
+    accent: colors.jacksonsPurple,
+    background: colors.cinder,
+    text: colors.selago,
   },
 };
