d228e91140bbb3c76fcd79defed8b72b23d71fd2
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-27 18:05:17 +0100

    changed per page to 8

--- src/features/Browse/getCoins.ts
+++ src/features/Browse/getCoins.ts
@@ -2,7 +2,7 @@ import { options } from "../../options";
 
 export const getCoins = async () => {
   const response = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&page=1`,
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=8&page=1`,
     options
   );
 
