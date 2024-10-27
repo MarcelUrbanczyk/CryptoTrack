f1cfb1782bedde2032d42d0583d0c0ad7e0fde52
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-27 22:04:48 +0100

    created theme slice

---
+++ src/common/themeSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../core/store";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: "light",
+  reducers: {
+    switchTheme: (state) => {
+      state = state === "light" ? "dark" : "light";
+    },
+  },
+});
+
+export const { switchTheme } = themeSlice.actions;
+export const selectTheme = (state: RootState) => state;
+
+export default themeSlice.reducer;
