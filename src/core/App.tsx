1bd85b12b35f060e8ea63529f75b7cbc98792770
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-27 18:05:06 +0100

    added new routes

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,24 +1,33 @@
-import Header from "../features/Header";
+import Navigation from "../features/Header";
 import {
   HashRouter,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom/cjs/react-router-dom";
-import { toExchanges, toBrowse } from "./routes";
+import { toExchanges, toBrowse, toNFTs, toTrade, toNews } from "./routes";
 import Browse from "../features/Browse";
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Header />
+        <Navigation />
         <Switch>
           <Route path={toBrowse()}>
             <Browse />
           </Route>
           <Route path={toExchanges()}>
             <>Exchanges</>
+          </Route>
+          <Route path={toNFTs()}>
+            <>NFTs</>
+          </Route>
+          <Route path={toTrade()}>
+            <>trade</>
+          </Route>
+          <Route path={toNews()}>
+            <>News</>
           </Route>
         </Switch>
       </HashRouter>
--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,3 +1,5 @@
 export const toExchanges = () => "/exchanges";
 export const toNews = () => "/news";
 export const toBrowse = () => "/browse";
+export const toTrade = () => "/trade";
+export const toNFTs = () => "/nfts";
