c7534293ddc7bafb639acaf70eb74591cdfb75ce
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-10-30 14:14:58 +0100

    added id to trade route

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,5 +1,5 @@
 export const toExchanges = () => "/exchanges";
 export const toNews = () => "/news";
 export const toBrowse = () => "/browse";
-export const toTrade = () => "/trade";
+export const toTrade = () => `/trade/:id`;
 export const toNFTs = () => "/nfts";
